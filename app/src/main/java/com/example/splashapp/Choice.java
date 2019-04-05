bb53b8f0b9cc43dbc43d08118b3ce13361b5e88b
Zhezlova Daria <zh.daria4@gmail.com>
2019-04-05 20:22:09 +0300

    Добавлены начальные заготовки под экраны, указанные в task-1

---
+++ app/src/main/java/com/example/splashapp/Choice.java
@@ -0,0 +1,30 @@
+package com.example.splashapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+
+public class Choice extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.choice);
+
+    }
+        public void CitChoice(View view)
+        {
+            Intent intent = new Intent(this, N_menu.class);
+            startActivity(intent);
+            finish();
+        }
+
+}
