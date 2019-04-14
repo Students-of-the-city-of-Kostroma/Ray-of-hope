316ee6de2690c35057ee710c44bd8473dbf1443d
SamuraSwordS <misterdzej@gmail.com>
2019-04-14 20:18:27 +0300

    Commit|1|:Controller/Citizen

---
+++ app/src/main/java/com/example/splashapp/C_Citizen.java
@@ -0,0 +1,12 @@
+package com.example.splashapp;
+
+public class C_Citizen extends ControllerUser{
+
+    public C_Citizen() { }
+
+    @Override
+    public User Update(User user) {
+
+        return user;
+    }
+}
