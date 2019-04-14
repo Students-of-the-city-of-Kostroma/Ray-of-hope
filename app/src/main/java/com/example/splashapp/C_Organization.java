e28c72b94f4fe09063aa63a961b62b1885ee1b40
SamuraSwordS <misterdzej@gmail.com>
2019-04-14 22:58:44 +0300

    Commit|1|:Organization

---
+++ app/src/main/java/com/example/splashapp/C_Organization.java
@@ -0,0 +1,11 @@
+package com.example.splashapp;
+
+public class C_Organization extends ControllerUser {
+
+    public C_Organization(){ }
+
+    @Override
+    public User Update(User user) {
+        return user;
+    }
+}
