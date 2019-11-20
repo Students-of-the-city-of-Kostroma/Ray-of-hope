7db1b8970c881a46d8a98ae02bf590d2306d8655
Стас Штауб <47520675+stasshtaub@users.noreply.github.com>
2019-11-20 09:32:27 +0300

    Исправление нерабочих кнопок "регистрация" и "вход"

--- WEB/public_html/pages/registration-organization.php
+++ WEB/public_html/pages/registration-organization.php
@@ -6,7 +6,7 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link href="../assets/css/normalize.css" rel="stylesheet">
     <link href="../assets/css/styles.css" rel="stylesheet">
-    <script src="../assets/js/jquery-3.4.1.min.js"></script>
+    <script type="text/javascript" src="http://code.jquery.com/jquery-1.10.0.min.js"></script>
     <script src="../assets/js/script.js"></script>
 	<script src="../assets/js/nav-mark.js"></script>
     <script src="../assets/js/ajax.js"></script>
