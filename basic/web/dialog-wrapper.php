82cfa4eb3d8a42ee980e33eb37f39051428998e9
Sergey <sepa33126@gmail.com>
2019-12-17 00:53:24 +0300

    Registration 67%

--- basic/web/dialog-wrapper.php
+++
@@ -1,7 +0,0 @@
-<div class="dialog-wrapper">
-    <dialog class="dialog-window">
-        <span class="dialog-title"></span>
-        <span class="dialog-message"></span>
-        <button class="dialog-ok-button" onclick="hide_dialog()">Ок</button>
-    </dialog>
-</div>
