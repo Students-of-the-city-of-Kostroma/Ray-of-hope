b9bff68e85b3b3bb04fdd9c39d7fc8b893749682
Sergey <sepa33126@gmail.com>
2019-12-16 18:24:02 +0300

    To server dir

---
+++ basic/models/EntryForm.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace app\models;
+
+use yii\base\Model;
+
+class EntryForm extends Model
+{
+    public $name;
+    public $email;
+
+    public function rules()
+    {
+        return [
+            [['name', 'email'], 'required'],
+            ['email', 'email'],
+        ];
+    }
+}
