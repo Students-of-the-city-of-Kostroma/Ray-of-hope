47c9ea65c987c11716872d93a775b04ff8fbec42
urec-programmec <sepa33126@gmail.com>
2020-01-11 22:19:35 +0300

    Recovering ubuntu, sorry

--- basic/views/registration/registration-citizen.php
+++ basic/views/registration/registration-citizen.php
@@ -12,34 +12,40 @@ use yii\widgets\ActiveForm;
 ?>
 
 
+ <script src="/assets/js/ajax.js"> </script>
 
 <section class="registration-org">
     <div class="container">
+<!-- "/index.php?r=registration-citizen%2Fcreate" -->
 
-        <form method="post" id="ajax_form" action="/index.php?r=registration-citizen%2Fcreate">
+        <form method="post" id="ajax_form" action="">
             <input type="hidden" name="_csrf" value="<?= \Yii::$app->request->getCsrfToken()?>">
             <p class="form-title">РЕГИСТРАЦИЯ ГРАЖДАНИНА</p>
             <div class="wrapper-textbox">
+
                 <div id="name" class="hint name"></div>
                 <img id="name" src="/assets/img/ico1.svg" alt="Некорректное имя" class="ico-in-textbox znak name" onmouseover="showHint(this)" onmouseout="hideHint(this)">
-
                 <input name="name" type="text" placeholder="Имя">
 
             </div>
             <div class="wrapper-textbox">
+
                 <div id="email" class="hint email"></div>
                 <img id="email" src="/assets/img/ico1.svg" alt="Некорректный E-mail" class="ico-in-textbox znak email" onmouseover="showHint(this)" onmouseout="hideHint(this)">
                 <input name="email" type="text" placeholder="E-mail">
+
             </div>
             <div class="wrapper-textbox">
+
                 <div id="password" class="hint password">От 6 символов: цифры и/или английские буквы</div>
                 <img id="password" src="/assets/img/ico2.svg" alt="Некорректный E-mail" class="ico-in-textbox tochki password" onmouseover="showHint(this)" onmouseout="hideHint(this)">
                 <input name="password" type="password" placeholder="Пароль">
+
             </div>
             <div class="wrapper-textbox">
                 <input name="password_2" type="password" placeholder="Повторите пароль">
             </div>
-            <input type="submit" id="registr-button" value="Зарегистрироваться">
+            <input type="button" id="registr-sitizen-button" value="Зарегистрироваться">
         </form>
     </div>
 </section>
