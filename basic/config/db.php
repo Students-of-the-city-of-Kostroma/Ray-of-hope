979bcf5ea508cc36dcabf7e7eab69d714f578988
Sergey <sepa33126@gmail.com>
2020-03-12 11:01:06 +0300

    Stable version №1

--- basic/config/db.php
+++ basic/config/db.php
@@ -4,7 +4,7 @@ return [
     'class' => 'yii\db\Connection',
     'dsn' => 'mysql:host=localhost;dbname=ray_of_hope',
     'username' => 'root',
-    'password' => '',
+    'password' => 'pass',
     'charset' => 'utf8',
 
     // Schema cache options (for production environment)
