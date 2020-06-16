a1e5d78c675e92d8eb343f93aa57d45a4946ceb9
Стас Штауб <shtaubstas@gmail.com>
2020-06-16 15:35:08 +0300

    fix

--- assets/js/header.js
+++ assets/js/header.js
@@ -1,7 +1,7 @@
 const moreMenu = document.querySelector('.nav-link.sub>.sub-menu');
 
 document.addEventListener('click', (e) => {
-    if (!e.target.matches('.nav-link.sub>a, .nav-link.sub>.sub-menu')) return;
+    if (!e.target.matches('.nav-link.sub>a, .nav-link.sub>.sub-menu, .nav-link.sub>.sub-menu>a')) return;
     if (e.target.matches('.nav-link.sub>a')) {
         e.preventDefault();
     }
